9fa973811127f71584e742d264bbda15e653237a
Epignosis <victoromelebele53@gmail.com>
2023-03-16 13:36:04 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio>
+/**
+ * main - A program that prints an output using the printf function
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
