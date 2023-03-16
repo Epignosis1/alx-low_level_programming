ee5d1c41397cae6153e8fd13e5b20555519944db
Epignosis <victoromelebele53@gmail.com>
2023-03-16 13:25:00 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - A program that uses the puts function to display an output
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzl");
+	return (0);
+}
