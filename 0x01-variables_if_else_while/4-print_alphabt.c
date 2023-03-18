ccfd12acb7bfaeba53eabb783f7ba39f72759de1
Epignosis <victoromelebele53@gmail.com>
2023-03-17 18:09:24 -0700

    Love

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry pointt
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c:
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+	if ((c == 'q' || c == 'e') != 1)
+	{
+	putchar(c);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
