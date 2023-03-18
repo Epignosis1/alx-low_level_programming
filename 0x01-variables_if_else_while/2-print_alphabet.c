3e84121805e056fc3ddbaf49dcf5ecec808b8b60
Epignosis <victoromelebele53@gmail.com>
2023-03-17 17:18:02 -0700

    Lower case letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - returns alphabets in lower and upper case
+ *
+ *  Returns: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a'
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	putchar("\n");
+	return (0);
+	}
+}
