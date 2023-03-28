121a46325f2d386272a8032383909bbf115926ed
Epignosis <victoromelebele53@gmail.com>
2023-03-28 06:14:32 -0700

    Print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _puts- that prints a string, followed by a new line, to stdout
+ * Return: string
+ * @str: it carries the string
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
