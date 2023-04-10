9e30630e09645b3121dba18b8f0ed3ad89759ed5
Epignosis <victoromelebele53@gmail.com>
2023-04-10 00:24:21 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main- a program that prints its name followed by a new line
+ * @argc: counts of argument in command line
+ * @argv: shows the strings
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	
+	return(0);
+}
