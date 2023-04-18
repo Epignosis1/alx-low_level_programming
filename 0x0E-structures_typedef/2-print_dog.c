138c12dd0b7131a8e11377d3abb1c9c144cb898b
Epignosis <victoromelebele53@gmail.com>
2023-04-18 15:39:57 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,13 +3,16 @@
 /**
  * print_dog- a function tha prints the struct dog
  * @d: carries argument
+ * Return; d is Null print (nil)
+ * if d is NULL print nothing
+ * if name is NULL print name: (nil)
  */
 
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", (d->name) ? d->name : "(nill)");
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
 		printf("Age: %f\n", (d->age) ? d->age : 0);
 		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
