9c183597035edf3e4a72d133b1828877f87e2f7b
Epignosis <victoromelebele53@gmail.com>
2023-04-17 17:23:20 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+#endif
+/**
+ * struct dog - defines a data type
+ * @name: The name of  the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
